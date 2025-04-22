3321becf5b49912a8ce4ec0b2b64a526e1d9913e
overloper <overloper0225@gmail.com>
2025-04-22 23:05:59 +0900

    feat: 사진 해상도 올리기

--- camera/main.py
+++ camera/main.py
@@ -1,1 +1,11 @@
-import gis
+import numpy as np
+import cv2
+
+img = cv2.imread("img.jpeg")
+sharpen_kernel = np.array([[0, -1, 0],
+                           [-1, 5,-1],
+                           [0, -1, 0]])
+sharpened = cv2.filter2D(img, -1, sharpen_kernel)
+
+denoised = cv2.fastNlMeansDenoisingColored(sharpened, None, 10, 10, 7, 21)
+cv2.imwrite("output_sharp_denoised.jpg", denoised)
